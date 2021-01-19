f899204d8648574ca300245bbf8a31989f6972e9
Ron Klinkien <ron@cyberjunky.nl>
2021-01-19 20:13:11 +0100

    Added name option to support multiple Toon's

--- custom_components/toon_boilerstatus/sensor.py
+++ custom_components/toon_boilerstatus/sensor.py
@@ -1,22 +1,30 @@
 """
-Support for reading Opentherm boiler status data using Toon thermostat's ketelmodule.
+Support for reading Smart Meter data using Toon thermostats meteradapter.
 Only works for rooted Toon.
 
 configuration.yaml
 
 sensor:
-  - platform: toon_boilerstatus
-    host: IP_ADDRESS
-    port: 80
-    scan_interval: 10
-    resources:
-      - boilersetpoint
-      - boilerintemp
-      - boilerouttemp
-      - boilerpressure
-      - boilermodulationlevel
-      - roomtemp
-      - roomtempsetpoint
+    - platform: toon_smartmeter
+        host: IP_ADDRESS
+        port: 80
+        scan_interval: 10
+        resources:
+            - gasused
+            - gasusedcnt
+            - elecusageflowpulse
+            - elecusagecntpulse
+            - elecusageflowlow
+            - elecusagecntlow
+            - elecusageflowhigh
+            - elecusagecnthigh
+            - elecprodflowlow
+            - elecprodcntlow
+            - elecprodflowhigh
+            - elecprodcnthigh
+            - elecsolar
+            - elecsolarcnt
+            - heat
 """
 import logging
 from datetime import timedelta
@@ -34,20 +42,28 @@ from homeassistant.const import (
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
-BASE_URL = 'http://{0}:{1}/boilerstatus/boilervalues.txt'
+BASE_URL = 'http://{0}:{1}/hdrv_zwave?action=getDevices.json'
 _LOGGER = logging.getLogger(__name__)
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=10)
 
 SENSOR_PREFIX = 'Toon '
 SENSOR_TYPES = {
-    'boilersetpoint': ['Boiler SetPoint', '°C', 'mdi:thermometer'],
-    'boilerintemp': ['Boiler InTemp', '°C', 'mdi:thermometer'],
-    'boilerouttemp': ['Boiler OutTemp', '°C', 'mdi:thermometer'],
-    'boilerpressure': ['Boiler Pressure', 'Bar', 'mdi:gauge'],
-    'boilermodulationlevel': ['Boiler Modulation', '%', 'mdi:fire'],
-    'roomtemp': ['Room Temp', '°C', 'mdi:thermometer'],
-    'roomtempsetpoint': ['Room Temp SetPoint', '°C', 'mdi:thermometer'],
+    'gasused': ['Gas Used Last Hour', 'm3', 'mdi:fire'],
+    'gasusedcnt': ['Gas Used Cnt', 'm3', 'mdi:fire'],
+    'elecusageflowpulse': ['Power Use', 'Watt', 'mdi:flash'],
+    'elecusageflowlow': ['P1 Power Use Low', 'Watt', 'mdi:flash'],
+    'elecusageflowhigh': ['P1 Power Use High', 'Watt', 'mdi:flash'],
+    'elecprodflowlow': ['P1 Power Prod Low', 'Watt', 'mdi:flash'],
+    'elecprodflowhigh': ['P1 Power Prod High', 'Watt', 'mdi:flash'],
+    'elecusagecntpulse': ['Power Use Cnt', 'kWh', 'mdi:flash'],
+    'elecusagecntlow': ['P1 Power Use Cnt Low', 'kWh', 'mdi:flash'],
+    'elecusagecnthigh': ['P1 Power Use Cnt High', 'kWh', 'mdi:flash'],
+    'elecprodcntlow': ['P1 Power Prod Cnt Low', 'kWh', 'mdi:flash'],
+    'elecprodcnthigh': ['P1 Power Prod Cnt High', 'kWh', 'mdi:flash'],
+    'elecsolar': ['P1 Power Solar', 'Watt', 'mdi:weather-sunny'],
+    'elecsolarcnt': ['P1 Power Solar Cnt', 'kWh', 'mdi:weather-sunny'],
+    'heat': ['P1 Heat', '', 'mdi:fire'],
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -58,10 +74,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Setup the Toon boilerstatus sensors."""
+    """Setup the Toon Smart Meter sensors."""
 
     session = async_get_clientsession(hass)
-    data = ToonBoilerStatusData(session, config.get(CONF_HOST), config.get(CONF_PORT))
+    data = ToonSmartMeterData(session, config.get(CONF_HOST), config.get(CONF_PORT))
     await data.async_update()
 
     entities = []
@@ -71,15 +87,15 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         unit = SENSOR_TYPES[resource][1]
         icon = SENSOR_TYPES[resource][2]
 
-        _LOGGER.debug("Adding Toon Boiler Status sensor: {}, {}, {}, {}".format(name, sensor_type, unit, icon))
-        entities.append(ToonBoilerStatusSensor(data, name, sensor_type, unit, icon))
+        _LOGGER.debug("Adding Toon Smart Meter sensor: {}, {}, {}, {}".format(name, sensor_type, unit, icon))
+        entities.append(ToonSmartMeterSensor(data, name, sensor_type, unit, icon))
 
     async_add_entities(entities, True)
 
-
 # pylint: disable=abstract-method
-class ToonBoilerStatusData(object):
+class ToonSmartMeterData(object):
     """Handle Toon object and limit updates."""
+
     def __init__(self, session, host, port):
         """Initialize the data object."""
 
@@ -106,21 +122,21 @@ class ToonBoilerStatusData(object):
             return
 
         try:
-            self._data = await response.json(content_type='text/plain')
+            self._data = await response.json(content_type='text/javascript')
             _LOGGER.debug("Data received from Toon: %s", self._data)
         except Exception as err:
             _LOGGER.error("Cannot parse data received from Toon: %s", err)
             self._data = None
-            return
 
     @property
     def latest_data(self):
         """Return the latest data object."""
-        return self._data
+        if self._data:
+            return self._data
+        return None
 
-
-class ToonBoilerStatusSensor(Entity):
-    """Representation of a Toon Boilerstatus sensor."""
+class ToonSmartMeterSensor(Entity):
+    """Representation of a Smart Meter connected to Toon."""
 
     def __init__(self, data, name, sensor_type, unit, icon):
         """Initialize the sensor."""
@@ -131,7 +147,8 @@ class ToonBoilerStatusSensor(Entity):
         self._icon = icon
 
         self._state = None
-        self._last_updated = None
+        self._discovery = False
+        self._dev_id = {}
 
     @property
     def name(self):
@@ -145,7 +162,7 @@ class ToonBoilerStatusSensor(Entity):
 
     @property
     def state(self):
-        """Return the state of the sensor."""
+        """Return the state of the sensor. (total/current power consumption/production or total gas used)"""
         return self._state
 
     @property
@@ -153,58 +170,139 @@ class ToonBoilerStatusSensor(Entity):
         """Return the unit of measurement of this entity, if any."""
         return self._unit
 
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes of this device."""
-        attr = {}
-        if self._last_updated is not None:
-            attr['Last Updated'] = self._last_updated
-        return attr
-
     async def async_update(self):
         """Get the latest data and use it to update our sensor state."""
 
         await self._data.async_update()
-        boiler = self._data.latest_data
+        energy = self._data.latest_data
 
-        if boiler:
-            if 'sampleTime' in boiler:
-              if boiler["sampleTime"] is not None:
-                self._last_updated = boiler["sampleTime"]
+        if energy:
+            if self._discovery == False:
+                for key in energy:
+                    dev = energy[key]
 
-            if self._type == 'boilersetpoint':
-              if 'boilerSetpoint' in boiler:
-                if boiler["boilerSetpoint"] is not None:
-                  self._state = float(boiler["boilerSetpoint"])
+                    """gas verbruik"""
+                    if dev['type'] in ['gas', 'HAE_METER_v2_1', 'HAE_METER_v3_1']:
+                        self._dev_id['gasused'] = key
+                        self._dev_id['gasusedcnt'] = key
 
-            elif self._type == 'boilerintemp':
-              if 'boilerInTemp' in boiler:
-                if boiler["boilerInTemp"] is not None:
-                  self._state = float(boiler["boilerInTemp"])
+                    """elec verbruik laag"""
+                    if dev['type'] in ['elec_delivered_lt', 'HAE_METER_v2_5', 'HAE_METER_v3_6', 'HAE_METER_v3_5']:
+                        self._dev_id['elecusageflowlow'] = key
+                        self._dev_id['elecusagecntlow'] = key
 
-            elif self._type == 'boilerouttemp':
-              if 'boilerOutTemp' in boiler:
-                if boiler["boilerOutTemp"] is not None:
-                  self._state = float(boiler["boilerOutTemp"])
+                    """elec verbruik hoog/normaal"""
+                    if dev['type'] in ['elec_delivered_nt', 'HAE_METER_v2_3', 'HAE_METER_v3_3', 'HAE_METER_v3_4']:
+                        self._dev_id['elecusageflowhigh'] = key
+                        self._dev_id['elecusagecnthigh'] = key
 
-            elif self._type == 'boilerpressure':
-              if 'boilerPressure' in boiler:
-                if boiler["boilerPressure"] is not None:
-                  self._state = float(boiler["boilerPressure"])
+                    """elec teruglevering laag"""
+                    if dev['type'] in ['elec_received_lt', 'HAE_METER_v2_6', 'HAE_METER_v3_7']:
+                        self._dev_id['elecprodflowlow'] = key
+                        self._dev_id['elecprodcntlow'] = key
 
-            elif self._type == 'boilermodulationlevel':
-              if 'boilerModulationLevel' in boiler:
-                if boiler["boilerModulationLevel"] is not None:
-                  self._state = float(boiler["boilerModulationLevel"])
+                    """elec teruglevering hoog/normaal"""
+                    if dev['type'] in ['elec_received_nt', 'HAE_METER_v2_4', 'HAE_METER_v3_5']:
+                        self._dev_id['elecprodflowhigh'] = key
+                        self._dev_id['elecprodcnthigh'] = key
 
-            elif self._type == 'roomtemp':
-              if 'roomTemp' in boiler:
-                if boiler["roomTemp"] is not None:
-                  self._state = float(boiler["roomTemp"])
+                _LOGGER.debug("Discovered these keys: %s", self._dev_id)
+                self._discovery = True
 
-            elif self._type == 'roomtempsetpoint':
-              if 'roomTempSetpoint' in boiler:
-                if boiler["roomTempSetpoint"] is not None:
-                  self._state = float(boiler["roomTempSetpoint"])
+                """gas verbruik laatste uur"""
+            if self._type == 'gasused':
+                if self._type in self._dev_id:
+                    self._state = float(energy[self._dev_id[self._type]]["CurrentGasFlow"])/1000
+
+                """gas verbruik teller laatste uur"""
+            elif self._type == 'gasusedcnt':
+                if self._type in self._dev_id:
+                    self._state = float(energy[self._dev_id[self._type]]["CurrentGasQuantity"])/1000
+
+                """elec verbruik puls"""
+            elif self._type == 'elecusageflowpulse':
+                if 'dev_3.2' in energy:
+                    self._state = energy["dev_3.2"]["CurrentElectricityFlow"]
+                elif 'dev_2.2' in energy:
+                    self._state = energy["dev_2.2"]["CurrentElectricityFlow"]
+                elif 'dev_4.2' in energy:
+                    self._state = energy["dev_4.2"]["CurrentElectricityFlow"]
+                elif 'dev_7.2' in energy:
+                    self._state = energy["dev_7.2"]["CurrentElectricityFlow"]
+
+                """elec verbruik teller puls"""
+            elif self._type == 'elecusagecntpulse':
+                if 'dev_3.2' in energy:
+                    self._state = float(energy["dev_3.2"]["CurrentElectricityQuantity"])/1000
+                elif 'dev_2.2' in energy:
+                    self._state = float(energy["dev_2.2"]["CurrentElectricityQuantity"])/1000
+                elif 'dev_4.2' in energy:
+                    self._state = float(energy["dev_4.2"]["CurrentElectricityQuantity"])/1000
+                elif 'dev_7.2' in energy:
+                    self._state = float(energy["dev_7.2"]["CurrentElectricityQuantity"])/1000
+
+                """elec verbruik laag"""
+            elif self._type == 'elecusageflowlow':
+                if self._type in self._dev_id:
+                    self._state = energy[self._dev_id[self._type]]["CurrentElectricityFlow"]
+
+                """elec verbruik teller laag"""
+            elif self._type == 'elecusagecntlow':
+                if self._type in self._dev_id:
+                    self._state = float(energy[self._dev_id[self._type]]["CurrentElectricityQuantity"])/1000
+
+                """elec verbruik hoog/normaal"""
+            elif self._type == 'elecusageflowhigh':
+                if self._type in self._dev_id:
+                    self._state = energy[self._dev_id[self._type]]["CurrentElectricityFlow"]
+
+                """elec verbruik teller hoog/normaal"""
+            elif self._type == 'elecusagecnthigh':
+                if self._type in self._dev_id:
+                    self._state = float(energy[self._dev_id[self._type]]["CurrentElectricityQuantity"])/1000
+
+                """elec teruglever laag"""
+            elif self._type == 'elecprodflowlow':
+                if self._type in self._dev_id:
+                    self._state = energy[self._dev_id[self._type]]["CurrentElectricityFlow"]
+
+                """elec teruglever teller laag"""
+            elif self._type == 'elecprodcntlow':
+                if self._type in self._dev_id:
+                    self._state = float(energy[self._dev_id[self._type]]["CurrentElectricityQuantity"])/1000
+
+                """elec teruglever hoog/normaal"""
+            elif self._type == 'elecprodflowhigh':
+                if self._type in self._dev_id:
+                    self._state = energy[self._dev_id[self._type]]["CurrentElectricityFlow"]
+
+                """elec teruglever teller hoog/normaal"""
+            elif self._type == 'elecprodcnthigh':
+                if self._type in self._dev_id:
+                    self._state = float(energy[self._dev_id[self._type]]["CurrentElectricityQuantity"])/1000
+
+                """zon op toon"""
+            elif self._type == 'elecsolar':
+                if 'dev_2.3' in energy:
+                    self._state = energy["dev_2.3"]["CurrentElectricityFlow"]
+                elif 'dev_3.3' in energy:
+                    self._state = energy["dev_3.3"]["CurrentElectricityFlow"]
+                elif 'dev_4.3' in energy:
+                    self._state = energy["dev_4.3"]["CurrentElectricityFlow"]
+
+                """zon op toon teller"""
+            elif self._type == 'elecsolarcnt':
+                if 'dev_2.3' in energy:
+                    self._state = float(energy["dev_2.3"]["CurrentElectricityQuantity"])/1000
+                elif 'dev_3.3' in energy:
+                    self._state = float(energy["dev_3.3"]["CurrentElectricityQuantity"])/1000
+                elif 'dev_4.3' in energy:
+                    self._state = float(energy["dev_4.3"]["CurrentElectricityQuantity"])/1000
+
+            elif self._type == 'heat':
+                if 'dev_2.8' in energy:
+                    self._state = float(energy["dev_2.8"]["CurrentHeatQuantity"])/1000
+                elif 'dev_4.8' in energy:
+                    self._state = float(energy["dev_4.8"]["CurrentHeatQuantity"])/1000
 
             _LOGGER.debug("Device: {} State: {}".format(self._type, self._state))
